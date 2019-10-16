53631009daf0ed49df05c8d41eef74ef1e8a3a19
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-10-16 11:41:08 -0400

    Create border.cpp

---
+++ Zork/border.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "border.h"
+
+Border::Border(XMLNode aNode) {
+  int i=0;
+  XMLNode bNode=aNode.getChildNode(i++);
+  do {
+    string tag=bNode.getName();
+    cout << "Border:" << tag << endl;
+    if (tag == "name") {
+      name=bNode.getText(0);
+    } else if (tag == "direction") {
+      direction=bNode.getText(0);
+      command=direction[0];  
+    }
+    bNode=aNode.getChildNode(i++);
+  } while (!bNode.isEmpty());
+}
+
